00488fd16e357a15175f7cc3c185f0f3d2351e7f
Marc Abraham VITALIS <marc-abraham.vitalis@amadeus.com>
2017-05-19 15:46:22 +0800

    use linux format in naming conventions

--- src/Management/Kuwarto.Management.Tests/Room.cs
+++ src/Management/Kuwarto.Management.Tests/Room.cs
@@ -4,7 +4,13 @@ namespace Kuwarto.Management.Tests
 {
     public class Room
     {
+
         public Room(string name)
+            : this(0, name)
+        {
+        }
+
+        public Room(int id, string name)
         {
             if (string.IsNullOrEmpty(name))
             {
@@ -12,8 +18,16 @@ namespace Kuwarto.Management.Tests
             }
 
             Name = name;
+
+            if (id == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            Id = id;
         }
 
         public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
